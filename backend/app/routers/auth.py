13aefed24c8c533857fd38c55319981ef3a4e792
slaweksystem <slaweksystem@gmail.com>
2024-11-13 14:12:25 +0100

    add tests

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -64,7 +64,7 @@ def authenticate_user(username: str, password: str, db: Session):
     user = db.query(Users).filter(Users.username == username).first()
     if not user:
         return False
-    print(f"founmd User {username}")
+    
     if not bcrypt_context.verify(password, user.hashed_password):
         return False
     return user
---
+++ backend/app/test/utils.py
@@ -0,0 +1,71 @@
+import pytest
+
+from sqlalchemy import create_engine, StaticPool, text
+from sqlalchemy.pool import StaticPool
+from sqlalchemy.orm import sessionmaker
+
+from fastapi.testclient import TestClient
+
+from ..main import app
+from ..database import Base
+from ..models import Projects, Users
+from ..routers.auth import bcrypt_context
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass = StaticPool,
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close
+
+def override_get_current_user():
+    return {'username': 'johnny', 'id':1, 'user_role': 'Admin'}
+
+client = TestClient(app)
+
+@pytest.fixture
+def test_Projects():
+    project = Projects(
+        title="First Sample Project",
+        description="This is a test project designed for testing purposes",
+        owner_id=1
+    )
+
+    db = TestingSessionLocal()
+    db.add(project)
+    db.commit()
+    yield project
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM projects;"))
+        connection.commit()
+
+@pytest.fixture
+def test_user():
+    user = Users(
+        username="johnny",
+        email="johnnybravo@example.com",
+        first_name="Johnny",
+        last_name="Bravo",
+        hashed_password=bcrypt_context.hash("1ubiepl@cki"),
+        role_id="1",
+    )
+    db = TestingSessionLocal()
+    db.add(user)
+    db.commit()
+    yield user
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM users;"))
+        connection.commit()
