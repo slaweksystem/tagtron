831216b63cfa690ca7bb64211270a703ff2d1ab0
KamilNowakk <Kamil.nowak9@interia.pl>
2024-12-21 11:10:26 +0100

    Podzielenie funkcji ekranow na osobne pliki konfiguracyjne

---
+++ frontend/src/AccountScreen.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const AccountScreen = ({ loggedInUser }) => {
+  return (
+    <div>
+      <p>Dane konta użytkownika:</p>
+      <ul>
+        <li>Login: {loggedInUser}</li>
+        <li>Data zalogowania: {new Date().toLocaleString()}</li>
+      </ul>
+      <button
+        style={{
+          padding: "10px 20px",
+          fontSize: "16px",
+          backgroundColor: "#28a745",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        Zmień hasłooo
+      </button>
+    </div>
+  );
+};
+
+export default AccountScreen;
