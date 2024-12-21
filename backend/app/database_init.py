3a0082ebd8be37c235716cb4f953ad8fceb5c6f6
slaweksystem <slaweksystem@gmail.com>
2024-12-21 11:20:51 +0100

    Add Project role

--- backend/app/database_init.py
+++ backend/app/database_init.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from .database import SessionLocal
-from .models import Roles
+from .models import Roles, ProjectRoles
 
 def add_role(db: Session,role: str):
     try:
@@ -16,8 +16,22 @@ def add_role(db: Session,role: str):
     finally:
         db.close()
 
+def add_project_role(db: Session,role: str):
+    try:
+        # Check if the default project role already exists
+        if not db.query(ProjectRoles).filter(ProjectRoles.name == role).first():
+            default_role = ProjectRoles(name=role)
+            db.add(default_role)
+            db.commit()
+            #print(f"{role} role created.") - add logger
+        else:
+            pass
+            #print(f"{role} role already exists.") add logger
+    finally:
+        db.close()
+
 async def init_db():
     """Initializes the database with default data, like the default role."""
     db = SessionLocal()
-    add_role(db, "User")
-    add_role(db, "Admin")
+    add_project_role(db, "User")
+    add_project_role(db, "Modder")
