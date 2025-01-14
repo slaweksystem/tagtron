29485230f91de43efc4cbc2c0e72cad9debd2ba6
slaweksystem <slaweksystem@gmail.com>
2025-01-14 23:30:05 +0100

    change database connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,23 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./tagtron.db'
+# PostgreSQL env
+PG_USERNAME = os.getenv('PG_USERNAME')
+PG_PASSWORD = os.getenv('PG_PASSWORD')
+PG_DATABASE = os.getenv('PG_DATABASE')
+PG_HOST = os.getenv('PG_HOST', 'localhost')  # Default host is 'localhost'
+PG_PORT = os.getenv('PG_PORT', '5432')       # Default port is '5432'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# PostgreSQL
+SQLALCHEMY_DATABASE_URL = f'postgresql://{PG_USERNAME}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DATABASE}'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# SQLite
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./tagtron.db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
